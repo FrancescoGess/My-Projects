31519c47f5c40307e74c964100a150704802ba4f
FrancescoGess <148055980+FrancescoGess@users.noreply.github.com>
2024-05-22 15:40:11 +0200

    add background page

---
+++ le-cinque-viti/src/components/Main/JumbotronSlides.vue
@@ -0,0 +1,54 @@
+<script>
+
+export default {
+  name: "JumbotronSlides",
+  data() {
+    return {
+      list: [
+        {
+          image: 'src/assets/img/slider1.jpg',
+          title: 'wine one'
+        },
+        {
+          image: 'src/assets/img/slider2.jpg',
+          title: 'wine two'
+        },
+        {
+          image: 'src/assets/img/slider3.jpg',
+          title: 'wine three'
+        },
+        {
+          image: 'src/assets/img/slider4.jpg',
+          title: 'wine four'
+        }
+      ],
+      timer: null,
+      currentIndex: 0
+    };
+  },
+  mounted: function () {
+
+  },
+  methods: {
+
+  }
+
+
+}
+
+</script>
+
+<template>
+  <div>
+    <figure>
+      <img id="wine-background" :src="list[0].image" alt="wine">
+    </figure>
+  </div>
+
+</template>
+
+<style lang="scss" scoped>
+#wine-background {
+  width: 100%;
+}
+</style>
