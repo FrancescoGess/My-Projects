6700fa92f1df033b8533b229743d977be7d40ed6
FrancescoGess <148055980+FrancescoGess@users.noreply.github.com>
2024-05-22 15:40:20 +0200

    add route

--- le-cinque-viti/src/App.vue
+++ le-cinque-viti/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Navbar from './components/Header/Navbar.vue'
+import JumbotronSlides from './components/Main/JumbotronSlides.vue'
 </script>
 
 <template>
 <Navbar></Navbar>
+<JumbotronSlides></JumbotronSlides>
 </template>
 
 <style scoped>
